39372f2478840a3f4b27defa69f59d6af793c60a
Ross Beazley <ross_beazley@discovery.com>
2022-02-23 15:07:42 +0000

    Use main not high profile

--- src/video.sh
+++ src/video.sh
@@ -12,7 +12,7 @@ rm -rf $BUILD/video_*
 for elem in "red","640x360" "blue","852x480" "yellow","1280x720" "indigo","1920x1080"
 do 
     IFS=',' read -a strarr <<< "${elem}"
-    ffmpeg -f lavfi -i color=${strarr[0]}:${strarr[1]}:d=600:rate=25,format=rgb24 $BUILD/video_${strarr[0]}_${strarr[1]}.mp4
+    ffmpeg -f lavfi -i color=${strarr[0]}:${strarr[1]}:d=600:rate=25,format=pix_fmts=yuv420p -c:v libx264 -profile:v main -level:v 3.0 $BUILD/video_${strarr[0]}_${strarr[1]}.mp4
 done
 
 
